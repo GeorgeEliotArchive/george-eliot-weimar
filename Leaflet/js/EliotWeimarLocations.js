67896212f0523237e6f51c9c66cd3460f4685622
W <72044648+wwl0007@users.noreply.github.com>
2024-04-21 09:02:15 -0500

    Create EliotWeimarLocations.js

---
+++ Leaflet/js/EliotWeimarLocations.js
@@ -0,0 +1,305 @@
+var EliotWeimarLocations = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3235, 50.9795 ]
+    },
+    "properties": {
+    "Date":"08/02/1853",
+    "Location":"Weimar Station",
+    "Info": "It was between three and four o’clock on a fine morning in August, that after a ten hours’ journey from Frankfort, I awoke at the Weimar station. No tipsiness can be more dead to all appeals than that which comes from fitful draughts of sleep on a railway journey by night. To the disgust of your wakeful companions, you are totally insensible to the existence of your umbrella, and to the fact that your carpet-bag is stowed under your seat, or that you have borrowed books and tucked them behind the cushion. »What’s the odds, so long as one can sleep?« is your philosophic formula; and it is not until you have begun to shiver on the platform in the early morning air that you become alive to property and its duties, – that is, to the necessity of keeping a fast grip upon it. Such was my condition when I reached the station at Weimar.<br><br>– from: George Eliot, Three Months in Weimar",
+    "MediaOne": "https://3.bp.blogspot.com/-BwQ-zUd9hfA/XJde18khO5I/AAAAAAAAA40/ifk6y2keybk0jekEDFIK41C3b30e-trCwCLcBGAs/s1600/Bahnabteil.jpg",
+    "MediaTwo": "<iframe allowfullscreen='allowfullscreen' class='b-hbp-video b-uploaded' frameborder='0' height='50' id='BLOGGER-video-3759120ef5001a09-16899' mozallowfullscreen='mozallowfullscreen' src='https://www.blogger.com/video.g?token=AD6v5dzc9q4l6TC6Jvy63hpQr_-RMFzUwp-vCAobUFer4zSJ2RMsab9w0BFzyiwK58dK0gH_hlMKJFo9Vvp3GDdaHCsnBdWxBpsgePbbJcgSyu5HAvBFdwmoklyNpmWmoHJWQcQ2SqFS' webkitallowfullscreen='webkitallowfullscreen' width='280'></iframe>",
+    "MediaThree": "http://2.bp.blogspot.com/-RaG0YqP6kYk/XJd1pEj0Z4I/AAAAAAAAA5E/_GgTAPs0NWAhFPywIpV40ObaVyfuQHvZwCK4BGAYYCw/s1600/passports.jpg",
+    "MediaFour": "https://4.bp.blogspot.com/-KCSQ614upEg/XJd1-gm84qI/AAAAAAAAA5M/GU837FJr_EMlr8mzJhPN-puNoq4OKSuxwCEwYBhgL/s1600/postamt.jpg",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3295, 50.9815 ]
+    },
+    "properties": {
+    "Date":"08/02/1854",
+    "Location":"Hotel zum Erbprinzen",
+    "Info":"… after a little difficulty we got some tolerable beds at the Erbprinz and slept away our fatigue by 9 o’clock. – from George Eliot’s Journal",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3335, 50.9835 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"Kaufstraße",
+    "Info":"They stopped in this medieval town on the way to Southern France. ",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3290, 50.9810 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"The Goethe Haus",
+    "Info":"From Lyon, they traveled down the Rhone to Avignon.",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3280, 50.9800 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"The Schiller Haus",
+    "Info":"They spent two days in Avignon.",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3265, 50.9785 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"Weimar Theatre",
+    "Info":"They passed through Marseilles to travel east along the coast toward Italy. ",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3250, 50.9770 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"The Stadtschloß",
+    "Info":"From Nice, they traveled via the Corniche (along the French coastline) into Italy.",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3230, 50.9755 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"The Altenburg",
+    "Info":"They spent a week in Genoa. ",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3200, 50.9735 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"The Ilm Park with Goethe’s Garden House",
+    "Info":"They passed through on the way to the Northern Italian lakes ",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3170, 50.9710 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"The Belvedere Schloß",
+    "Info":"They spent several days on the Northern Italian lakes. ",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3140, 50.9685 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"The Ettersburg",
+    "Info":"They spent several days on the Northern Italian lakes. ",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3110, 50.9660 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"The Tiefurt Schloß",
+    "Info":"They returned from Italy to Switzerland via Simplon Pass.",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3080, 50.9635 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"Bad Berka",
+    "Info":"They continued via the Col de Balme to Chamonix on the North side of the Mont Blanc on horseback, with the ultimate goal of Geneva.",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3050, 50.9605 ]
+    },
+    "properties": {
+    "Date":"08/1854",
+    "Location":"The Kickelhahn Hut",
+    "Info":"They arrives in Geneva four days after their departure from Chamonix. After a week on the Northern shore of the lake, they returned here, and the Brays took leave of their friend on July 25 to return home to Coventry. Eliot stayed behind in a pension, Campagne Plongeon, and then moved into lodgings with the painter D'Albert Durade and his wife on October 9. She stayed all winter; on March 18, 1850, she began her return trip to England accompanied by M. D'Albert Durade. ",
+    "MediaOne": "",
+    "MediaTwo": "",
+    "MediaThree": "",
+    "MediaFour": "",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.3235, 50.9795 ]
+    },
+    "properties": {
+    "Date":"08/02/1853",
+    "Location":"Weimar Station",
+    "Info": "It was between three and four o’clock on a fine morning in August, that after a ten hours’ journey from Frankfort, I awoke at the Weimar station. No tipsiness can be more dead to all appeals than that which comes from fitful draughts of sleep on a railway journey by night. To the disgust of your wakeful companions, you are totally insensible to the existence of your umbrella, and to the fact that your carpet-bag is stowed under your seat, or that you have borrowed books and tucked them behind the cushion. What’s the odds, so long as one can sleep? is your philosophic formula; and it is not until you have begun to shiver on the platform in the early morning air that you become alive to property and its duties, – that is, to the necessity of keeping a fast grip upon it. Such was my condition when I reached the station at Weimar. – from: George Eliot, Three Months in Weimar",
+    "MediaOne": "<img src=\"https://3.bp.blogspot.com/-BwQ-zUd9hfA/XJde18khO5I/AAAAAAAAA40/ifk6y2keybk0jekEDFIK41C3b30e-trCwCLcBGAs/s1600/Bahnabteil.jpg\" alt=\"Train Compartment\" style=\"max-width:500px; width:100%\">",
+    "MediaTwo": "<iframe allowfullscreen='allowfullscreen' class='b-hbp-video b-uploaded' frameborder='0' height='210' id='BLOGGER-video-3759120ef5001a09-16899' mozallowfullscreen='mozallowfullscreen' src='https://www.blogger.com/video.g?token=AD6v5dzc9q4l6TC6Jvy63hpQr_-RMFzUwp-vCAobUFer4zSJ2RMsab9w0BFzyiwK58dK0gH_hlMKJFo9Vvp3GDdaHCsnBdWxBpsgePbbJcgSyu5HAvBFdwmoklyNpmWmoHJWQcQ2SqFS' webkitallowfullscreen='webkitallowfullscreen' width='280'></iframe>",
+    "MediaThree": "http://2.bp.blogspot.com/-RaG0YqP6kYk/XJd1pEj0Z4I/AAAAAAAAA5E/_GgTAPs0NWAhFPywIpV40ObaVyfuQHvZwCK4BGAYYCw/s1600/passports.jpg",
+    "MediaFour": "https://4.bp.blogspot.com/-KCSQ614upEg/XJd1-gm84qI/AAAAAAAAA5M/GU837FJr_EMlr8mzJhPN-puNoq4OKSuxwCEwYBhgL/s1600/postamt.jpg",
+    "MediaFive": "",
+    "MediaSix": "",
+    "MediaSeven": "",
+    "MediaEight": "",
+    }
+  }
+]
+};
